3820ce5c6d53cda6d75647b6f0d19361a9d46302
alexanderpulov <alec2918@gmail.com>
2023-10-09 22:22:53 +0300

    css-modules

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import s from "./Header.module.css";
+
 
 const Header = () => {
     return (
-        <header className={"header"}>
+        <header className={s.header}>
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBtXRxuEjJz7Kci75j4K4_tPKNc_l1yfEMSw" alt=""/>
         </header>
     );
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from "./Profile.module.css";
 
 const Profile = () => {
     return (
-        <div className={"content"}>
+        <div className={s.content}>
             <div><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/NYC_Downtown_Manhattan_Skyline_seen_from_Paulus_Hook_2019-12-20_IMG_7347_FRD_%28cropped%29.jpg/800px-NYC_Downtown_Manhattan_Skyline_seen_from_Paulus_Hook_2019-12-20_IMG_7347_FRD_%28cropped%29.jpg?20211109035035" alt=""/></div>
             <div>ava + description</div>
             <div>My post
